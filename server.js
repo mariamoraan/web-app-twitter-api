64ec78ac6a8e97e984cb5ffc60b42df47c266338
mia-moran <mariamoranluaces@gmail.com>
2021-11-16 20:10:33 +0100

    ll

--- server.js
+++ server.js
@@ -44,6 +44,7 @@ dotenv.config({path:'./.env'});
 // This middleware informs the express application to serve our compiled React files
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
     app.use(express.static(path.join(__dirname, 'client/build')));
+    dotenv.config({path:path.join(__dirname, './.env')});
     app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
